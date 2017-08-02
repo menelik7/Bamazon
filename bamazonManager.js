4dbbfa595e6bf9b6c1f8bf34299c76deef263fbc
Menelik Tefera <menelikworku@gmail.com>
2017-08-01 23:48:18 -0400

    edited to conceal code

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,15 +1,14 @@
 //Copyright Menelik Tefera 2017
 //menelikworku@gmail.com
 //GT Coding Bootcamp
-//Bamazon
+//Bamazon 
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var result = [];
 var item = "";
 var stock = 0;
-var idContainer = [];
-var chosenItem = false;
+var newQty = 0;
 require("console.table");
 
 var connection = mysql.createConnection({
@@ -27,8 +26,10 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId);
-  console.log("\n\rHello dear customer - here is a list of available items: \n\r");
-  customerView();
+  console.log("\n\rWelcome back Mr. Manager!\n\r");
+  menuOptions();
 });
 
 IF YOU WISH TO SEE THE REST OF THIS CODE, CONTACT THE OWNER!
+
+
